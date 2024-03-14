41f0648483cd0c46e4cccb220ed33c3f98e667f4
LDplay <58827820+LDplay@users.noreply.github.com>
2024-03-14 15:51:55 +0200

    Add files via upload

---
+++ HW2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+
+class Program
+{
+
+    static void Task1()
+    {
+        string childProcessPath = @"C:\Windows\System32\notepad.exe";
+
+        Process childProcess = new Process();
+        childProcess.StartInfo.FileName = childProcessPath;
+
+        try
+        {
+            childProcess.Start();
+
+            childProcess.WaitForExit();
+
+            int exitCode = childProcess.ExitCode;
+            Console.WriteLine($"Дочірній процес завершився з кодом {exitCode}.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка: {ex.Message}");
+        }
+        finally
+        {
+            childProcess.Close();
+        }
+    }
+    static void Task2()
+    {
+        string childProcessPath = @"C:\Windows\System32\notepad.exe";
+
+        Process childProcess = new Process();
+        childProcess.StartInfo.FileName = childProcessPath;
+
+        try
+        {
+            // Запускаємо дочірній процес
+            childProcess.Start();
+
+            Console.WriteLine("Оберіть дію:");
+            Console.WriteLine("1. Чекати на завершення дочірнього процесу");
+            Console.WriteLine("2. Примусово завершити дочірній процес");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            { 
+                childProcess.WaitForExit();
+
+                int exitCode = childProcess.ExitCode;
+                Console.WriteLine($"Дочірній процес завершився з кодом {exitCode}.");
+            }
+            else if (choice == "2")
+            {
+                childProcess.Kill();
+                Console.WriteLine("Дочірній процес був примусово завершений.");
+            }
+            else
+            {
+                Console.WriteLine("Невірний вибір.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка: {ex.Message}");
+        }
+        finally
+        {
+            childProcess.Close();
+        }
+    }
+
+    
+
+
+    static void Main()
+    {
+        //Task1();
+
+        Task2();
+    }
+}
+
